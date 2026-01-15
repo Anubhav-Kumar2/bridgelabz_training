a1d9aa0bebfab31f977e9117244063b30ac33d37
Anubhav-Kumar2 <anubhavkatiyar9876@gmail.com>
2026-01-15 14:34:01 +0530

    added index.js

---
+++ bridgelabz-training/index.js
@@ -0,0 +1,24 @@
+console.log(" node.js backend started");
+//2 variable
+let name = "jay";  //string
+let age=22;              //number
+const country="India";   //constant
+let isstudent = true ;   //boolean
+
+
+console.log(name);
+console.log(age);
+console.log(country);
+console.log(isstudent);
+
+
+
+
+
+
+
+
+
+
+
+
