e80f0ba9d8ed24c85cb63b4c9af9ffa48a456417
Anubhav-Kumar2 <anubhavkatiyar9876@gmail.com>
2026-01-15 14:44:26 +0530

    last file

--- bridgelabz-training/first.js
+++ bridgelabz-training/first.js
@@ -1,1 +1,14 @@
 console.log("Anubhav");
+//2 variable
+let name = "jay";  //string
+let age=22;              //number
+const country="India";   //constant
+let isstudent = true ;   //boolean
+
+
+console.log(name);
+console.log(age);
+console.log(country);
+console.log(isstudent);
+
+
