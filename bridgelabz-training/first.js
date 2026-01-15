eb8b492bbaecc95a32fe1b0300f52d501c333862
Anubhav-Kumar2 <anubhavkatiyar9876@gmail.com>
2026-01-15 14:54:32 +0530

    third

--- bridgelabz-training/first.js
+++ bridgelabz-training/first.js
@@ -11,4 +11,21 @@ console.log(age);
 console.log(country);
 console.log(isstudent);
 
+//6 operators
+let a=10;
+let b=5;
+
+console.log("Add:",a+b);
+console.log("Add:",a-b);
+console.log("Add:",a*b);
+console.log("Add:",a/b);
+
+//condition (if-else)
+let loginAge=18;
+if(loginAge>=18){
+    console.log("Allowed to login")
+}    
+else {
+    console.log("Not Allowed")
+}
 
